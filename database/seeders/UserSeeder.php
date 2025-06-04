e02dcf21e350e6feec312716bb30921a4c09223d
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2025-06-05 00:09:07 +0700

    Perbarui nomor telepon pengguna dan tambahkan pengguna baru di UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
                 'nama' => 'Eko Muchamad Haryono',
                 'email' => 'eko@demo.com',
                 'password' => Hash::make('password'),
-                'nomorTelepon' => '081234567890',
+                'nomorTelepon' => '082246105463',
                 'peran' => 'admin',
                 'alamat' => 'Jakarta',
                 'foto_profil' => 'foto_profil/default.png', // default foto profil
@@ -24,7 +24,7 @@ class UserSeeder extends Seeder
                 'nama' => 'Firenze Higa Putra',
                 'email' => 'firenze@demo.com',
                 'password' => Hash::make('password'),
-                'nomorTelepon' => '081234567891',
+                'nomorTelepon' => '085894310722',
                 'peran' => 'admin',
                 'alamat' => 'Bandung',
                 'foto_profil' => 'foto_profil/default.png', // default foto profil
@@ -33,7 +33,7 @@ class UserSeeder extends Seeder
                 'nama' => 'Fatiya Labibah',
                 'email' => 'fatiya@demo.com',
                 'password' => Hash::make('password'),
-                'nomorTelepon' => '081234567892',
+                'nomorTelepon' => '083877136044',
                 'peran' => 'mentor',
                 'alamat' => 'Surabaya',
                 'foto_profil' => 'foto_profil/default.png', // default foto profil
@@ -51,7 +51,7 @@ class UserSeeder extends Seeder
                 'nama' => 'Ahmad Faiz Al Asad',
                 'email' => 'faiz@demo.com',
                 'password' => Hash::make('password'),
-                'nomorTelepon' => '081234567894',
+                'nomorTelepon' => '082214283126',
                 'peran' => 'admin',
                 'alamat' => 'Semarang',
                 'foto_profil' => 'foto_profil/default.png', // default foto profil
@@ -60,7 +60,7 @@ class UserSeeder extends Seeder
                 'nama' => 'Arby Ali Amludin',
                 'email' => 'arby@demo.com',
                 'password' => Hash::make('password'),
-                'nomorTelepon' => '081234567895',
+                'nomorTelepon' => '081295563905',
                 'peran' => 'pelanggan',
                 'alamat' => 'Malang',
                 'foto_profil' => 'foto_profil/default.png', // default foto profil
@@ -69,9 +69,36 @@ class UserSeeder extends Seeder
                 'nama' => 'Muhammad Wildan Ziyad Alfarabi',
                 'email' => 'wildan@demo.com',
                 'password' => Hash::make('password'),
-                'nomorTelepon' => '081234567896',
+                'nomorTelepon' => '081299666275',
                 'peran' => 'pelanggan',
                 'alamat' => 'Depok',
+                'foto_profil' => 'foto_profil/default.png', // default foto profil
+            ],
+            [
+                'nama' => 'Eko Muchamad Haryono',
+                'email' => 'ekopelanggan@demo.com',
+                'password' => Hash::make('password'),
+                'nomorTelepon' => '082246105463',
+                'peran' => 'pelanggan',
+                'alamat' => 'Jakarta',
+                'foto_profil' => 'foto_profil/default.png', // default foto profil
+            ],
+            [
+                'nama' => 'Firenze Higa Putra',
+                'email' => 'firenzepelanggan@demo.com',
+                'password' => Hash::make('password'),
+                'nomorTelepon' => '085894310722',
+                'peran' => 'admin',
+                'alamat' => 'Bandung',
+                'foto_profil' => 'foto_profil/default.png', // default foto profil
+            ],
+            [
+                'nama' => 'Muhammad Farrel Zilviano',
+                'email' => 'farrelpelanggan@demo.com',
+                'password' => Hash::make('password'),
+                'nomorTelepon' => '081234567893',
+                'peran' => 'admin',
+                'alamat' => 'Yogyakarta',
                 'foto_profil' => 'foto_profil/default.png', // default foto profil
             ],
         ];
@@ -103,18 +130,13 @@ class UserSeeder extends Seeder
             ];
         }
 
-        // 10 pelanggan tambahan dengan nama, email, dan kota Indonesia acak
+        // 5 pelanggan tambahan dengan nama, email, dan kota Indonesia acak
         $namaPelanggan = [
             'Rina Wulandari',
             'Andi Saputra',
             'Sari Dewi',
             'Dian Prasetyo',
             'Yusuf Maulana',
-            'Putri Ayu',
-            'Rizky Ramadhan',
-            'Lina Marlina',
-            'Bayu Nugroho',
-            'Tia Rahmawati',
         ];
         $kotaPelanggan = [
             'Bandung',
@@ -122,13 +144,8 @@ class UserSeeder extends Seeder
             'Medan',
             'Makassar',
             'Palembang',
-            'Semarang',
-            'Depok',
-            'Bekasi',
-            'Bogor',
-            'Malang',
         ];
-        for ($i = 1; $i <= 10; $i++) {
+        for ($i = 1; $i <= 5; $i++) {
             $nama = $namaPelanggan[$i - 1];
             $namaEmail = strtolower(str_replace(' ', '', $nama));
             $users[] = [
