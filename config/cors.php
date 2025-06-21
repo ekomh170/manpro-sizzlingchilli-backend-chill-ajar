f2dad5c834e229dbb9a6ed3a99aacbe21e80b361
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2025-06-21 15:03:27 +0700

    feat: Menambahkan pengaturan CORS dan middleware untuk mengizinkan akses dari domain lain

---
+++ config/cors.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173', // untuk dev React lokal
+        'https://chill-ajar.vercel.app',
+        'https://chill-ajar-pentest.vercel.app',
+        'https://manpro-sizzlingchilli-backend-chill-ajar.onrender.com',
+        'http://35.188.34.254', // akses langsung ke IP server
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
