5797b8db0839d99d216bfc839500d25de3175296
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2025-06-23 22:34:07 +0700

    fix: perbarui allowed_origins untuk menggunakan HTTPS pada IP server

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
         'https://chill-ajar.vercel.app',
         'https://chill-ajar-pentest.vercel.app',
         'https://manpro-sizzlingchilli-backend-chill-ajar.onrender.com',
-        'http://35.188.34.254', // akses langsung ke IP server
+        'https://35.188.34.254', // akses langsung ke IP server
         'https://chillajar.my.id', // akses langsung ke domain frontend
         'https://peladen.my.id' // akses langsung ke domain backend
     ],
